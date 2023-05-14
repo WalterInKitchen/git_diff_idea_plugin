d5e9823d47bb6fe7fb8ec4bceae2a096a697620f
walter <walterinkitchen@outlook.com>
2023-05-14 17:16:18 +0800

    使用jgit对比workingTree与head

--- src/main/java/top/walterInKitchen/gitdiff/git/JgitUnCommitChangesProvider.java
+++ src/main/java/top/walterInKitchen/gitdiff/git/JgitUnCommitChangesProvider.java
@@ -4,13 +4,21 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.treewalk.AbstractTreeIterator;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.FileTreeIterator;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+
+import static org.eclipse.jgit.lib.Constants.HEAD;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
@@ -20,20 +28,25 @@ public class JgitUnCommitChangesProvider implements UnCommitChangesProvider {
     @Override
     public DiffStat getUnCommittedChanged() {
         try {
-            List<DiffEntry> entries = git.diff()
-                    .setCached(true)
-                    .call();
+            Repository repository = git.getRepository();
+            AbstractTreeIterator oldTree = getOldTree(repository);
+            AbstractTreeIterator newTree = new FileTreeIterator(repository);
 
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             DiffFormatter diffFormatter = new DiffFormatter(out);
-            diffFormatter.setRepository(git.getRepository());
+            diffFormatter.setRepository(repository);
+            List<DiffEntry> entries = diffFormatter.scan(oldTree, newTree);
 
             int addLine = 0;
             int delLine = 0;
             int files = 0;
 
             for (DiffEntry entry : entries) {
-                diffFormatter.format(entry);
+                try {
+                    diffFormatter.format(entry);
+                } catch (MissingObjectException exception) {
+                    continue;
+                }
                 files++;
             }
 
@@ -59,9 +72,21 @@ public class JgitUnCommitChangesProvider implements UnCommitChangesProvider {
                     .setInsertions(addLine)
                     .setDeletions(delLine)
                     .build();
-        } catch (GitAPIException | IOException exc) {
+        } catch (IOException exc) {
             throw new RuntimeException(exc);
         }
+    }
+
+    private AbstractTreeIterator getOldTree(Repository repository) throws IOException {
+        ObjectId head = repository.resolve(HEAD + "^{tree}"); //$NON-NLS-1$
+        if (head == null) {
+            throw new RuntimeException("no head found");
+        }
+        CanonicalTreeParser oldTree = new CanonicalTreeParser();
+        try (ObjectReader reader = repository.newObjectReader()) {
+            oldTree.reset(reader, head);
+        }
+        return oldTree;
     }
 
     public static JgitUnCommitChangesProvider build(String basePath) {
